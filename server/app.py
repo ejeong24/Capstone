dea2d568399cc7f5dff3495e1fa9eb0895b1f94f
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-07-06 19:41:39 -0400

    made first successful request to FutDB API

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 # server/app.py
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, request, session, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from config import app, db, api, Resource
 #from models import *insert Models*
 import ipdb
+import requests
 
 CORS(app)
 migrate = Migrate(app, db)
@@ -13,6 +14,15 @@ migrate = Migrate(app, db)
 @app.route('/')
 def index():
     return 'Welcome to FutHut'
+
+@app.route('/players', methods=['GET'])
+def get_players():
+    url = 'https://futdb.app/api/players'
+    headers = {
+        'X-AUTH-TOKEN': 'e0218f1b-c550-4938-a8d5-e309e6dc02b7'
+    }
+    response = requests.get(url, headers=headers)
+    return response.content
 
 if __name__ == '__main__':
     app.run(port=5555)
