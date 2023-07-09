83868871f1686670effd9c3faa64c9c641f5e884
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-07-09 19:04:50 -0400

    updated backend route logic

--- server/app.py
+++ server/app.py
@@ -15,21 +15,23 @@ def index():
 
 @app.route('/users/register', methods=['POST'])
 def register():
-    username = request.json['username']
-    email = request.json['email']
-    password = request.json['password']
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
 
     # Perform user registration logic and save to database
     user = User(username=username, email=email, password=password)
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({'message': 'User registered successfully'})
+    return {'message': 'User registered successfully'}
 
 @app.route('/users/login', methods=['POST'])
 def login():
-    username = request.json['username']
-    password = request.json['password']
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
 
     # Perform user login logic and session management
     # You can add your own logic here to verify the username and password
@@ -37,9 +39,9 @@ def login():
     user = User.query.filter_by(username=username, password=password).first()
     if user:
         session['user_id'] = user.id
-        return jsonify({'message': 'User logged in successfully'})
+        return {'message': 'User logged in successfully'}
     else:
-        return jsonify({'message': 'Invalid username or password'})
+        return {'message': 'Invalid username or password'}
 
 @app.route('/players', methods=['GET'])
 def players():
@@ -61,21 +63,22 @@ def players_by_id(playerId):
 
 @app.route('/squads', methods=['POST'])
 def create_squad():
-    squad_name = request.json['squad_name']
-    user_id = request.json['user_id']
+    data = request.get_json()
+    squad_name = data.get('squad_name')
+    user_id = data.get('user_id')
 
     # Perform squad creation logic and save to database
     squad = Squad(name=squad_name, userId=user_id)
     db.session.add(squad)
     db.session.commit()
 
-    return jsonify({'message': 'Squad created successfully'})
+    return {'message': 'Squad created successfully'}
 
 @app.route('/users/<string:userID>/squads', methods=['GET'])
 def get_user_squads(userID):
     user = User.query.get(userID)
     squads = [squad.serialize() for squad in user.squads]
-    return jsonify({'squads': squads})
+    return {'squads': squads}
 
 @app.route('/squads/<string:squadID>', methods=['PATCH'])
 def edit_squad(squadID):
@@ -86,7 +89,7 @@ def edit_squad(squadID):
     squad.name = new_squad_name
     db.session.commit()
 
-    return jsonify({'message': 'Squad edited successfully'})
+    return {'message': 'Squad edited successfully'}
 
 @app.route('/squads/<string:squadID>', methods=['DELETE'])
 def delete_squad(squadID):
@@ -95,7 +98,7 @@ def delete_squad(squadID):
     db.session.delete(squad)
     db.session.commit()
 
-    return jsonify({'message': 'Squad deleted successfully'})
+    return {'message': 'Squad deleted successfully'}
 
 @app.route('/users/<string:userID>/profile', methods=['DELETE'])
 def delete_user_profile(userID):
@@ -104,13 +107,14 @@ def delete_user_profile(userID):
     db.session.delete(user)
     db.session.commit()
 
-    return jsonify({'message': 'User profile deleted successfully'})
+    return {'message': 'User profile deleted successfully'}
 
 @app.route('/users/logout', methods=['POST'])
 def logout():
     # Code for user logout and session management
     session.pop('user_id', None)
-    return jsonify({'message': 'User logged out successfully'})
+    
+    return {'message': 'User logged out successfully'}
 
 if __name__ == '__main__':
     app.run(port=5555)
