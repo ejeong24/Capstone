bcf1d9ecfa7f56c5301904513193726f6091aadf
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-07-11 00:35:52 -0400

    leagues populating on front end properly

--- client/src/components/LeagueList.jsx
+++ client/src/components/LeagueList.jsx
@@ -3,39 +3,47 @@ import React, { useEffect, useState } from 'react';
 // LeagueList component
 function LeagueList({ handleLeagueClick }) {
   const [leagues, setLeagues] = useState([]);
-  const [page, setPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    fetchLeagues();
-  }, [page]);
+    fetchLeagues(currentPage);
+  }, [currentPage]);
 
-  const fetchLeagues = () => {
-    fetch(`https://futdb.app/api/leagues?page=${page}`)
+  const fetchLeagues = (page) => {
+    fetch(`/leagues?page=${page}`)
       .then(response => response.json())
-      .then(data => setLeagues(data))
+      .then(data => {
+        setLeagues(data.leagues);
+        setTotalPages(data.pagination.pageTotal);
+      })
       .catch(error => console.error(error));
   };
 
   const handleNextPage = () => {
-    setPage(page + 1);
+    setCurrentPage(prevPage => prevPage + 1);
   };
 
   const handlePreviousPage = () => {
-    setPage(page - 1);
+    setCurrentPage(prevPage => prevPage - 1);
   };
 
   return (
     <div>
       <ul>
-        {leagues.map(league => (
-          <li key={league.id}>
-            {league.name}
-            <button onClick={() => handleLeagueClick(league.id)}>View Players</button>
+        {leagues.map((league, index) => (
+          <li key={index}>
+            {league}
+            <button onClick={() => handleLeagueClick(index)}>View Players</button>
           </li>
         ))}
       </ul>
-      <button onClick={handlePreviousPage} disabled={page === 1}>Previous Page</button>
-      <button onClick={handleNextPage}>Next Page</button>
+      <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+        Previous Page
+      </button>
+      <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+        Next Page
+      </button>
     </div>
   );
 }
