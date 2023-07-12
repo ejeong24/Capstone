cf7e96da52a1632fc0f59b060ffd83066aade5bd
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-07-12 11:07:02 -0400

    small fix to leagues

--- client/src/components/LeagueList.jsx
+++ client/src/components/LeagueList.jsx
@@ -14,6 +14,7 @@ function LeagueList({ handleLeagueClick }) {
     fetch(`/leagues?page=${page}`)
       .then(response => response.json())
       .then(data => {
+        console.log(data);
         setLeagues(data.leagues);
         setTotalPages(data.pagination.pageTotal);
       })
